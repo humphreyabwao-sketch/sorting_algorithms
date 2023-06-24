2654f558aad38ccc1a5d6d16f0988b4b53737ebf
EvansOdhams <evansodhams@gmail.com>
2023-06-24 15:21:42 +0300

    Edit sort.h

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,42 @@
 #include "sort.h"
 
 /**
-* selection_sort - Sorts an array of integers in ascending order
-*                  using the Selection sort algorithm
-* @array: The array to be sorted
-* @size: The size of the array
+* selection_sort - sorts an array of integers in ascending order
+* using the Selection sort algorithm
+* prints the array after each time you swap two elements
+*
+* @array: array of integers to be sorted
+* @size: number of integers in array
 */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_idx;
+size_t i, j, j_min;
+int temp;
 
-if (array == NULL || size < 2)
+/* An array does not need to be sorted if its size is less than 2 */
+if (size < 2)
 return;
 
-for (i = 0; i < size - 1; i++)
+for (i = 0; i < size; i++)
 {
-min_idx = i;
+j_min = i;
+
 for (j = i + 1; j < size; j++)
 {
-if (array[j] < array[min_idx])
-min_idx = j;
+/* find the smallest integer in the rest of the array */
+if (array[j] < array[j_min])
+j_min = j;
 }
 
-if (min_idx != i)
-swap(&array[i], &array[min_idx]);
+/* put the smallest value in the beginning of the unsorted part */
+if (j_min != i)
+{
+temp = array[i];
+array[i] = array[j_min];
+array[j_min] = temp;
 
+/* print the array */
 print_array(array, size);
+}
 }
 }
