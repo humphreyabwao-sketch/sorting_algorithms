7bd07fc83fcbfb70c4fee9481a9cc7622a442973
EvansOdhams <evansodhams@gmail.com>
2023-06-24 16:23:53 +0300

    Add 104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,60 +1,66 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
-* heapify - Adjusts the heap to satisfy the heap property
-* @array: Array of integers
-* @size: Size of the array
-* @index: Index of the current node
-* @max_size: Size of the array (for printing purposes)
+* swap - utility function to swap two integers
+* @a: integer a
+* @b: integer b
 */
-void heapify(int *array, size_t size, size_t index, size_t max_size)
+void swap(int *a, int *b)
 {
-size_t largest = index;
-size_t left = 2 * index + 1;
-size_t right = 2 * index + 2;
-int temp;
+int t = *a;
+*a = *b;
+*b = t;
+}
 
-if (left < size && array[left] > array[largest])
+/**
+* maxHeapify - The main function to heapify a Max Heap.
+* The function assumes that everything under the given root
+* (element at index idx) is already heapified.
+* @array: array
+* @size: size of the array for print
+* @idx: index
+* @n: size of the array to run
+*/
+void maxHeapify(int *array, size_t size, int idx, size_t n)
+{
+int largest = idx;
+int left = 2 * idx + 1;
+int right = 2 * idx + 2;
+
+if (left < (int)n && array[left] > array[largest])
 largest = left;
 
-if (right < size && array[right] > array[largest])
+if (right < (int)n && array[right] > array[largest])
 largest = right;
 
-if (largest != index)
+if (largest != idx)
 {
-temp = array[index];
-array[index] = array[largest];
-array[largest] = temp;
-
-print_array(array, max_size);
-
-heapify(array, size, largest, max_size);
+swap(&array[idx], &array[largest]);
+print_array(array, size);
+maxHeapify(array, size, largest, n);
 }
 }
 
 /**
-* heap_sort - Sorts an array of integers in ascending order using Heap sort
-* @array: Array of integers
-* @size: Size of the array
+* heap_sort - The main function to sort an array of given size
+* @array: array to sort
+* @size: size of the array
 */
 void heap_sort(int *array, size_t size)
 {
-int i, temp;
+int i;
 
 if (array == NULL || size < 2)
 return;
 
-for (i = size / 2 - 1; i >= 0; i--)
-heapify(array, size, i, size);
+for (i = (size - 2) / 2; i >= 0; --i)
+maxHeapify(array, size, i, size);
 
-for (i = size - 1; i > 0; i--)
+for (i = (size - 1); i > 0; --i)
 {
-temp = array[0];
-array[0] = array[i];
-array[i] = temp;
-
+swap(&array[0], &array[i]);
 print_array(array, size);
-
-heapify(array, i, 0, size);
+maxHeapify(array, size, 0, i);
 }
 }
