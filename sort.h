028604a4a7c5e5d74b46517e2cf6733f21577935
EvansOdhams <evansodhams@gmail.com>
2023-06-24 13:48:47 +0300

    Edit Sort.h

--- sort.h
+++ sort.h
@@ -1,24 +1,25 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
-/* 0-bubble_sort.c */
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
