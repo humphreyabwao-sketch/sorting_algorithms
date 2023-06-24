fc7585d29c5626431662ef70102ce9a6729170aa
EvansOdhams <evansodhams@gmail.com>
2023-06-24 15:18:24 +0300

    Edit sort.h

--- sort.h
+++ sort.h
@@ -4,17 +4,17 @@
 #include <stddef.h>
 
 /**
-* struct listint_s - Doubly linked list node
-*
-* @n: Integer stored in the node
-* @prev: Pointer to the previous element of the list
-* @next: Pointer to the next element of the list
-*/
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
@@ -22,13 +22,9 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-<<<<<<< HEAD
 void selection_sort(int *array, size_t size);
-void swap(int *a, int *b);
-=======
 void shell_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
@@ -37,6 +33,5 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
->>>>>>> 5dc689b60fc08bc5da64264c2940015234e55b11
 
 #endif /* SORT_H */
